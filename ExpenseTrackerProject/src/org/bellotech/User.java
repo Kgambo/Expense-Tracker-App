51d812cf9511bbbf2d901dd9c1a8d87d909216fa
Rochinas <Rochinas@Rochinas-Laptop>
2023-10-03 18:20:32 +0100

    second commit.

--- ExpenseTrackerProject/src/org/bellotech/User.java
+++ ExpenseTrackerProject/src/org/bellotech/User.java
@@ -1,5 +1,23 @@
 package org.bellotech;
 
-public class User {
 
+public class User{
+	public String userID;
+	public String email;
+	public String password;
+	public String address;
+	public int phoneNumb;
+	
+	
+	public User(String userID, String email, String password, String address, int phoneNumb) {
+
+		this.userID = userID;
+		this.email = email;
+		this.password = password;
+		this.address = address;
+		this.phoneNumb = phoneNumb;
+	}
+	
+	
 }
+
