547d5c5dae85bfea530f4a1850b27d6c4a0f96b5
Rochinas <Rochinas@Rochinas-Laptop>
2023-10-03 19:03:00 +0100

    Auth done.

--- ExpenseTrackerProject/src/org/bellotech/User.java
+++ ExpenseTrackerProject/src/org/bellotech/User.java
@@ -1,5 +1,6 @@
 package org.bellotech;
 
+import java.util.Scanner;
 
 public class User{
 	public String userID;
@@ -7,17 +8,84 @@ public class User{
 	public String password;
 	public String address;
 	public int phoneNumb;
+	public int setPassword;
 	
 	
-	public User(String userID, String email, String password, String address, int phoneNumb) {
+	public User(String userID, String email, String password, String address, int phoneNumb,int setPassword) {
 
 		this.userID = userID;
 		this.email = email;
 		this.password = password;
 		this.address = address;
 		this.phoneNumb = phoneNumb;
+		this.setPassword =  setPassword;
+		
+		
 	}
 	
-	
+	public String getUserID() {
+		return userID;
+	}
+
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public int getPhoneNumb() {
+		return phoneNumb;
+	}
+
+	public void setPhoneNumb(int phoneNumb) {
+		this.phoneNumb = phoneNumb;
+	}
+
+	public int getSetPassword() {
+		return setPassword;
+	}
+
+	public void setSetPassword(int setPassword) {
+		this.setPassword = setPassword;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public void authCheck() {
+		char ch;
+		do {
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Enter username");
+			String username = sc.nextLine();		
+			System.out.println("Enter password");
+			String setPassword = sc.nextLine();
+			if (username.equals(userID)  && setPassword.equals(setPassword)) {
+				System.out.println("Welcome User");
+				break;
+			}else {
+				System.out.println("Access Denied");
+			}
+			System.out.println("Do you want to continue? " + "Y for yes/ N for no");
+			
+			 ch = sc.next().charAt(0);
+			
+	}while(ch == "Y"  || ch == "Y");
+	}
 }
 
